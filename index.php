f0e8ca22b04c9924e2c2b427c191ffa2959d63d3
root <root@localhost>
2015-04-25 09:29:01 -0700

    completed symptoms, working on table headers

--- index.php
+++ index.php
@@ -52,11 +52,15 @@ since the injury or following the Game which is
 not usually noted with Rugby.
 </h4>
 <table style="width 100%">
+   <tr>
+      <th>How many</br>
+     Identify any symptom you have experienced since the injury or following the Game which is not usually noted with Rugby.</th>
+  </tr>
   <tr>
     <td>
      <div>
 				<label>
-				  <span>Headaches &nbsp &nbsp &nbsp</span>
+				  <span>Headaches</span>
 				</label>
 			</div>
 			</td>
@@ -68,7 +72,7 @@ not usually noted with Rugby.
     <td>
       <div>
 				<label>
-					<span>'Pressure in head' &nbsp &nbsp &nbsp</span>
+					<span>'Pressure in head'</span>
 				</label>
 			</div>
 	  </td>
@@ -80,12 +84,240 @@ not usually noted with Rugby.
      <td>
       <div>
 				<label>
-					<span>Neck pain &nbsp &nbsp &nbsp</span>
+					<span>Neck pain</span>
 				</label>
 		  </div>
 		</td>
 		<td>
 			  <input type="checkbox" name="symptom[]" value="neck pain">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Nausea or vomiting</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="nausea">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Dizziness</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="dizziness">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Blurred vision</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="blurred vision">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Balance problems</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="balance problems">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Sensitivity to light</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="sensitivity to light">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Feeling slowed down</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="feeling slowed down">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Sensitivity to noise</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="sensitivity to noise">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Feeling like "in a fog"</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="feeling like in a fog">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>"Don't feel right"</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="don't feel right">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Difficulty concentrating</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="difficulty concentrating">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Difficulty remembering</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="difficulty remembering">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Fatigue or low energy</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="fatigue">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Confusion</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="confusion">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Drowsiness</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="drowsiness">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Trouble falling asleep/disturbed sleep</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="trouble falling asleep">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>More emotional</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="more emotional">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Irritability</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="irritability">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Sadness</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="sadness">
+		</td>
+  </tr>
+    <tr>
+     <td>
+      <div>
+				<label>
+					<span>Nervous or anxious</span>
+				</label>
+		  </div>
+		</td>
+		<td>
+			  <input type="checkbox" name="symptom[]" value="nervousness">
 		</td>
   </tr>
 </table>
